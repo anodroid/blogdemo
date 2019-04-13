ae6d8cd75c02aa3b209523646dfb6a22bf2ad2cd
laurence <liangxingluo@163.com>
2019-04-13 13:42:25 +0800

    modify migrations manually

--- blog/migrations/0004_auto_20190330_2213.py
+++ blog/migrations/0004_auto_20190330_2213.py
@@ -15,4 +15,16 @@ class Migration(migrations.Migration):
             old_name='create_date',
             new_name='created_date',
         ),
+        migrations.CreateModel(
+            name='Visit',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('visit_date', models.DateField(auto_now_add=True, default=django.utils.timezone.now, verbose_name='访问日期'),)
+                ('count', models.IntegerField(default=0, verbose_name='日访问量')),
+            ],
+            options={
+                'verbose_name': '日访问量',
+                'verbose_name_plural': '日访问量',
+            },
+        ),
     ]
